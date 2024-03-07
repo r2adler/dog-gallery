5fbd3ef69068fb259aa614d728889e57e8def318
Artur Khareksian <r2mail1122@gmail.com>
2024-03-07 13:19:06 +0300

    3

--- app/all-dogs/page.tsx
+++ app/all-dogs/page.tsx
@@ -3,15 +3,15 @@ import { fetchDogs } from "../actions";
 
 const MAX_DOGS = 50;
 
-export const AllDogs = async () => {
+export const AllDogsPage = async () => {
   const dogs = await fetchDogs(MAX_DOGS);
 
   return (
     <div className="flex flex-col items-center justify-center bg-white dark:bg-gray-800 py-2 min-h-screen">
-      AllDogs
+      <h2>AllDogs </h2>
       <DogGallery dogImages={dogs} />
     </div>
   );
 };
 
-export default AllDogs;
+export default AllDogsPage;
